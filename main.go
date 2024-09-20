109e0d116f43a94496b2d17297fdb16d2d281a3d
Baptiste Hardy <baptiste@hardy.sh>
2024-09-20 21:31:08 +0200

    feat: First defined tokens

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type TokenType string
+
+const (
+	TOKEN_METADATA_START   = "METADATA_START"
+	TOKEN_METADATA_END     = "METADATA_END"
+	TOKEN_METADATA_KEY     = "METADATA_KEY"
+	TOKEN_METADATA_VALUE   = "METADATA_VALUE"
+	TOKEN_COMMENT          = "COMMENT"
+	TOKEN_ROLE             = "ROLE"
+	TOKEN_PERSON           = "PERSON"
+	TOKEN_PERSON_ALIAS     = "PERSON_ALIAS"
+	TOKEN_BRAND            = "BRAND"
+	TOKEN_ENTRY            = "ENTRY"
+	TOKEN_COMMA            = "COMMA"
+	TOKEN_ASSIGNMENT_VALUE = "ASSIGNMENT_VALUE"
+	TOKEN_ASSIGNMENT_START = "ASSIGNMENT_START"
+	TOKEN_ASSIGNMENT_END   = "ASSIGNMENT_END"
+	TOKEN_EOF              = "EOF"
+)
+
+type Token struct {
+	Type  TokenType
+	Value string
+}
 
 func main() {
-    fmt.Println("sakugo")
+	fmt.Println("sakugo")
 }
